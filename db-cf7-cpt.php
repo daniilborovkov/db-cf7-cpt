8c65089ec7e208b506785841cb4c0c00ef610cd8
Daniil Borovkov <daniilborowkow@gmail.com>
2018-10-16 15:12:41 +0600

    Initial commit

---
+++ db-cf7-cpt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Db Cf7 Cpt
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     db-cf7-cpt
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Db_Cf7_Cpt
+ */
+
+// Your code starts here.
